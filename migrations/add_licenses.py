10a9699bee8bc485a3938d137fb48f87c721bb6e
ReeseHatfield <reesehatfield02@gmail.com>
2026-09-02 18:45:03 -0400

    migrate test database under licenses

---
+++ migrations/add_licenses.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+import json
+from pathlib import Path
+
+path = "./courses-test" # change me to migrate test
+for course in Path(path).iterdir():
+    oseda_conf_file = course / "oseda-config.json"
+
+    if not oseda_conf_file.exists():
+        print("Could not find oseda-config.json for ", course)
+        continue
+
+    with open(oseda_conf_file) as f:
+        j = json.load(f)
+
+    if "license" not in j:
+        j["license"] = "Apache-2.0"
+
+    with open(oseda_conf_file, "w") as f:
+        json.dump(j, f, indent=2)
