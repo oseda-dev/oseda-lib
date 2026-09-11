f9887073bc4875b16d01f669ed17e57ee5e1e986
ReeseHatfield <reesehatfield02@gmail.com>
2026-09-11 08:26:08 -0400

    Add course: CS-3180-02-Methods-of-Execution

---
+++ courses/CS-3180-02-Methods-of-Execution/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    
+    public static int add(int a, int b){
+        return a + b;
+    }
+    
+    public static void main(String[] args){
+        int result = add(1, 2);
+        System.out.println("Result was: " + result);
+    }
+}
